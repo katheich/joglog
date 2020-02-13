346b539b2850b1b703946e2f4e79855ac50f8816
Kathrin Eichinger <kathrin801@gmail.com>
2020-02-13 14:05:27 +0000

    add postgresql

---
+++ joglog/schema.py
@@ -0,0 +1,15 @@
+import graphene
+
+import links.schema
+import users.schema
+
+
+class Query(links.schema.Query, graphene.ObjectType):
+    pass
+
+
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType,):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- joglog/settings.py
+++ joglog/settings.py
@@ -75,13 +75,14 @@ WSGI_APPLICATION = 'joglog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'runs',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
