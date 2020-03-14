37b6380bdc623443b65c8e4069baabdeeb950812
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-14 15:34:08 +0000

    removed email as required field

--- users/schema.py
+++ users/schema.py
@@ -15,12 +15,10 @@ class CreateUser(graphene.Mutation):
     class Arguments:
         username = graphene.String(required=True)
         password = graphene.String(required=True)
-        email = graphene.String(required=True)
 
-    def mutate(self, info, username, password, email):
+    def mutate(self, info, username, password):
         user = get_user_model()(
             username=username,
-            email=email,
         )
         user.set_password(password)
         user.save()
