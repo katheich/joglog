29aa92ab64b0ba0b39366f3156ef977cf063e46e
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-14 17:55:40 +0000

    homepage design

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,18 +4,14 @@ import React from 'react'
 // const infoType = 'user'
 
 const Home = () => (
-  <div className="section">
-    <div className="container">
-      <div className="hero is-fullheight">
-
-        <div className="hero-body">
-          <div className="container">
-            <div className="title is-size-1">Jog Log</div>
-          </div>
-        </div>
+  <section className="hero is-fullheight has-text-right" id="home">
+    <div className="hero-title">
+      <div className="container">
+        <div className="title">Jog Log</div>
       </div>
     </div>
-  </div>
+    <div className="shadow"></div>
+  </section>
 )
 
 export default Home
