f969a87747962f41a50d1b131dd2ca6d0149e67b
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-14 16:42:03 +0000

    fullstack setup - yarn, frontend folder, webpack, babel

---
+++ frontend/src/app.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App = () => {
+  
+
+  return <div>Jog Log</div>
+}
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
