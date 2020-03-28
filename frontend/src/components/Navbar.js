f53fe5d3918331113e29478f3575f46e19c4c992
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-28 15:53:53 +0000

    navbar design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
-
 import Auth from '../lib/auth'
 
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const ME_QUERY = gql`
+  {
+    me {
+      username
+    }
+
+  }        
+`
 const Navbar = (props) => {
 
   function handleLogout(e) {
@@ -13,11 +23,27 @@ const Navbar = (props) => {
   }
 
   if (!Auth.isAuthorized()) return <></>
-  return (<div className="container">
+  return (<div className="" id="navbar">
     <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
       <navbar className="start">
         <div className="navbar-item">
-            Hello
+          <div className="title">
+            <Query query={ME_QUERY}>
+              {({ loading, error, data }) => {
+                if (loading) {
+                  return null
+                }
+                if (error) {
+                  
+                  return <span>(no username found)</span>
+                }
+                
+                return <span>{data.me.username}&apos;s</span> 
+                
+              }}
+            </Query>
+            <span> Jog Log</span>
+          </div>
         </div>
       </navbar>
       <div className="navbar-end">
