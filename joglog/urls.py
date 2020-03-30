bb4cebb9360c046544503d5633d8e63386b8b33a
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-30 18:47:47 +0100

    hookup frontend to heroku

--- joglog/urls.py
+++ joglog/urls.py
@@ -22,5 +22,5 @@ from .schema import schema
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/api', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
-        path('', include('frontend.urls'))
+    path('', include('frontend.urls'))
 ]
