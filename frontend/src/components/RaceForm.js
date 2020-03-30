ccbc76ac13e91496819332547287d854e6e92b5d
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-30 16:14:07 +0100

    races immediate update

--- frontend/src/components/RaceForm.js
+++ frontend/src/components/RaceForm.js
@@ -28,11 +28,12 @@ const DELETE_MUTATION = gql`
   mutation deleteRace($id: ID!) {
     deleteRace (id: $id) {
       ok
+      id
     }
   }
 `
 
-const RaceForm = ( { date, modalDate, toggleModal, race }) => {
+const RaceForm = ( { date, modalDate, toggleModal, race, updateInfo }) => {
 
   const initialInfo = {
     description: '',
@@ -59,8 +60,10 @@ const RaceForm = ( { date, modalDate, toggleModal, race }) => {
     console.log(info)
   }
 
-  const confirm = () => {
+  const confirm = (data, operation) => {
+    console.log('DATA RECEIVED IN RESPONSE', data)
     toggleModal()
+    updateInfo(operation, 'races', data)
   }
 
   useEffect(() => {
@@ -101,15 +104,15 @@ const RaceForm = ( { date, modalDate, toggleModal, race }) => {
         </div>
       </div>
 
-      {race ? <></> : <><Mutation mutation={POST_MUTATION} variables={{ ...info }} onCompleted={data => confirm(data)} onError={err => setErrors(err.message)}>
+      {race ? <></> : <><Mutation mutation={POST_MUTATION} variables={{ ...info }} onCompleted={data => confirm(data.createRace, 'create')} onError={err => setErrors(err.message)}>
         {postMutation => <button onClick={postMutation} className="button is-primary"><i className="fas fa-check"></i></button>}
       </Mutation>
       {errors && <small className="help is-danger">{errors}</small>}
       </>}  
-      {race ? <><Mutation mutation={EDIT_MUTATION} variables={{ ...info }} onCompleted={data => confirm(data)} onError={err => setErrors(err.message)}>
+      {race ? <><Mutation mutation={EDIT_MUTATION} variables={{ ...info }} onCompleted={data => confirm(data.editRace, 'update')} onError={err => setErrors(err.message)}>
         {editMutation => <button onClick={editMutation} className="button is-primary"><i className="fas fa-check"></i></button>}
       </Mutation>
-      <Mutation mutation={DELETE_MUTATION} variables={{ id: info.id }} onCompleted={data => confirm(data)} onError={err => setErrors(err.message)}>
+      <Mutation mutation={DELETE_MUTATION} variables={{ id: info.id }} onCompleted={data => confirm(data.deleteRace, 'delete')} onError={err => setErrors(err.message)}>
         {deleteMutation => <button onClick={deleteMutation} className="button is-danger"><i className="fas fa-trash-alt"></i></button>}
       </Mutation>
       {errors && <small className="help is-danger">{errors}</small>}
