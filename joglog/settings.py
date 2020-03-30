3dcb6207de0e3842e442e084cd44f5870170e762
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-30 18:33:48 +0100

    Ready for deployment

--- joglog/settings.py
+++ joglog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,5 @@ GRAPHENE = {
         'graphql_jwt.middleware.JSONWebTokenMiddleware',
     ],
 }
+
+django_heroku.settings(locals())
