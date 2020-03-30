4d9b1febbfc819eb11ac189580838936f39490d8
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-30 18:45:57 +0100

    hookup frontend to heroku

--- joglog/settings.py
+++ joglog/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'runs'
+    'runs',
+    'frontend'
+
 ]
 
 MIDDLEWARE = [
--- joglog/urls.py
+++ joglog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from .schema import schema
@@ -22,4 +22,5 @@ from .schema import schema
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/api', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+        path('', include('frontend.urls'))
 ]
