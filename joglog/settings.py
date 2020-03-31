094077a2cd09db41c709f227cb0a2035c7a3846f
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-31 10:28:47 +0100

    debug off

--- joglog/settings.py
+++ joglog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+o1#s0l8j=&z2na&aj))4wa-o)*6m(6c$ngzaz2s-31omei##&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
